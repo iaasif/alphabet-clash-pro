44575bbfd53a778b9e6ce6e53810549055c48535
iaasif <imtiazasif.cse@gmail.com>
2024-02-18 18:52:46 +0600

    pushing

--- js/alpha.js
+++ js/alpha.js
@@ -7,29 +7,44 @@ function getARandomAlphabet() {
 
     const randomNumber = Math.random() * 25;
     const index = Math.round(randomNumber);
-    console.log(alphabets);
-    console.log(index);
+    // console.log(alphabets);
+    // console.log(index);
+
+    const alphabet = alphabets[index];
+    console.log(index, alphabet);
+    return alphabet;
+
 }
 
 function play() {
-    console.log("object");
+    // console.log("object");
     const homeSection = document.getElementById('home-screen');
     homeSection.classList.add("hidden");
     // console.log(homeSection.classList);
     const playgroundSection = document.getElementById('playground-secition');
     playgroundSection.classList.remove('hidden');
-    console.log("object");
+    // console.log("object");
 
 
-    continueGame();  
+    continueGame();
 
 }
 
 function continueGame() {
     const alphabet = getARandomAlphabet()
-    console.log(alphabet);
+    console.log('your new alpha is' + alphabet);
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet
+
+    setBackgroundColorById(alphabet); 
 }
 
+function setBackgroundColorById(elemnetId) {
+    const element = document.getElementById(elemnetId);
+    element.classList.add('bg-orange-400');
+
+}
 
 // continueGame()
 
